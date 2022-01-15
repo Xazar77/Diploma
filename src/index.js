77a98449495ef12cf07d84bbac25c359125b6729
Anderey Prokhorov <beta737@yandex.ru>
2022-01-15 21:59:50 +0300

    smoothScroll

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import slider from './modules/slider';
 import timer from './modules/timer';
 import swiper from './modules/swiper';
 import form from './modules/form';
+import smoothScroll from './modules/smoothScroll';
 
 
 
@@ -15,10 +16,11 @@ modalFroze();
 slider();
 timer('26 january 2022');
 swiper();
-form({
-    formId: '.order-form', 
-    someElement: [{
-        type: 'block',
-        id: 'total'
-    }],
-});
+// form({
+//     formId: '.order-form', 
+//     someElement: [{
+//         type: 'block',
+//         id: 'total'
+//     }],
+// });
+smoothScroll();
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,27 @@
+
+const smoothScroll = () => {
+
+    const scrollBtn = document.querySelector('.smooth-scroll');
+    scrollBtn.style.display = 'none';  
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > 400) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    });
+   
+    scrollBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log(e.target)
+        document.getElementById('header').scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        });
+        
+        
+    });
+  
+
+};
+export default smoothScroll;
