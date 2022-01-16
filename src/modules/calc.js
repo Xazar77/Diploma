c6d6b38fbdccc78981dd6475786154ed99268dfa
Anderey Prokhorov <beta737@yandex.ru>
2022-01-16 16:38:23 +0300

    Правка form и calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -15,18 +15,19 @@ const calc = () => {
     let interval;
     let totalValue = 0;
 
-    const checkInput = (input) => {
-        input.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/\D+/, '');
-            
-        });
-    };
-    checkInput(calcSquare);
+    // const checkInput = (input) => {
+        
+    // };
+    // checkInput(calcSquare);
     
     const countCalc = () => {
        
         const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
         const calcTypeMaterialValue = +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value;
+        calcSquare.addEventListener('input', (e) => {
+            calcSquare.value = e.target.value.replace(/\D+/, '');
+            
+        });
         const calcSquareValue = calcSquare.value;
        
         if (calcTypeValue && calcTypeMaterialValue && calcSquareValue) {
@@ -41,41 +42,40 @@ const calc = () => {
     };
     
     
-    calcBlock.addEventListener('input', (e) => {
-        if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcSquare ) {
-             
-            clearTimeout(interval);
+    if (calcBlock) {
+        calcBlock.addEventListener('input', (e) => {
+            if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcSquare) {
+                 
+                clearTimeout(interval);
+                    
+                countCalc();
                 
-            countCalc();
-            
-            
-            const time = 1000; // время отводимое на перебор цифр
-            const step = 100; // шаг перебора
-            let n = 0; // число отсчитывается от нуля
-            let t = Math.round(time / +totalValue / step);
-
-            if (totalValue != 0) {
                 
-                interval = setTimeout(() => {
-                    animate({
-                        duration: 500,
-                        timing(timeFraction) {
-                            return timeFraction;
-                        },
-                        draw(progress) {
-                            calcTotal.placeholder = Math.round(progress * totalValue) + '₽';
-                        }
-                    });
-                },200);
+                const time = 1000; // время отводимое на перебор цифр
+                const step = 100; // шаг перебора
+                let n = 0; // число отсчитывается от нуля
+                let t = Math.round(time / +totalValue / step);
+    
+                if (totalValue != 0) {
+                    
+                    interval = setTimeout(() => {
+                        animate({
+                            duration: 500,
+                            timing(timeFraction) {
+                                return timeFraction;
+                            },
+                            draw(progress) {
+                                calcTotal.placeholder = Math.round(progress * totalValue) + '₽';
+                            }
+                        });
+                    }, 200);
+                }
+                
             }
-               
-                
-             console.log(totalValue)    
-            
-            
-        }
-    });
+        });
+    }
 
+    
 };
 
 export default calc;
