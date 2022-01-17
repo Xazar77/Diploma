07079859b730a0044cb312ae157dc288c66f9d77
Anderey Prokhorov <beta737@yandex.ru>
2022-01-17 11:45:26 +0300

    правка form

--- src/modules/form.js
+++ src/modules/form.js
@@ -4,7 +4,12 @@ import { validate } from "./helper";
 const form = (formId) => {
 
     const forms = document.querySelectorAll(formId);
+    const inputName = document.querySelectorAll('[name=fio]');
+    const inputPhone = document.querySelectorAll('[name=phone]');
+   
     
+    
+  
 
     const statusBlock = document.createElement('div'),
         statusImg = document.createElement('img');
@@ -12,20 +17,51 @@ const form = (formId) => {
         errorText = 'Ошибка...',
         successText = 'Спасибо! Наш менеджер свами свяжется!';
     statusBlock.style.cssText = `
-                font-size: 20px;
-                color: red;
-                text-align:center;
+        font-size: 20px;
+        color: red;
+        text-align:center;
     `;
         
-        statusImg.style.cssText = `
-            color: yellow;
-            display: block;
-            margin: 0 auto;
-            text-align:center;
+    statusImg.style.cssText = `
+        color: yellow;
+        display: block;
+        margin: 0 auto;
+        text-align:center;
     `;
 
+    const validName = (list) => {
+        list.forEach(name => {
+            name.addEventListener('input', (e) => {
 
+                let testText = /^[а-яёa-z]{2,}/gi;
 
+                if (testText.test(e.target.value)) {
+                    name.value = e.target.value;
+                } 
+                name.value = e.target.value.replace(/^[^а-яёa-z]+/gi, '');
+            });
+        });
+    };
+    validName(inputName);
+
+    const validPhone = (list) => {
+        list.forEach(phone => {
+            phone.addEventListener('input', (e) => {
+
+                let testPhone = /^([+]?[0-9]{6,16})$/i;
+                console.log(testPhone.test(e.target.value));
+                if (testPhone.test(e.target.value)) {
+                    phone.value = e.target.value;
+                } else {
+                    phone.value = e.target.value.replace(/[а-яa-z]+/g, '').replace(/\-+/g, '-').replace(/[^\d\(\)\-\+]+/g, '').replace(/^[\-\s]+/gm, '').replace(/[\-\s]+$/gm, '');
+                }
+
+            });
+
+        });
+
+    };
+    validPhone(inputPhone);
     
 
        const sendData = (data) => {
@@ -60,12 +96,27 @@ const form = (formId) => {
                 formData.forEach((val, key) => {
                     formBody[key] = val;
                 });
+                if (document.getElementById('calc-total')) {
+                    const element = document.getElementById('calc-total');
+                    let total = 'Итого за работу:';
+                    if (element.placeholder == 'Итого') {
+                        element.placeholder = 0;
+                         
+                    } else {
+                        formBody[total] = element.placeholder;
+                    }
+               
+               }
+        
 
                 if (validate(inputList)) {
                     sendData(formBody)
                         .then(data => {
-                        statusBlock.textContent = successText;
-                        statusImg.src = '';
+                            statusBlock.textContent = successText;
+                            statusImg.src = '';
+                            setTimeout(() => {
+                                statusBlock.textContent = '';
+                            }, 1000);
 
                             inputList.forEach(input => {
                                 input.value = '';
@@ -90,11 +141,6 @@ const form = (formId) => {
 
 
     
-
-    
-
-
-
 
 };
 export default form;
